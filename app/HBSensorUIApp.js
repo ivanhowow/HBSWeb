86defbbf26a18cfbee290b7ef4fe8bb48b45a446
Ivan Ho <ivanho11221122@gmail.com>
2013-06-13 12:46:12 +0800

    added groundwork css and canvasjs for charting

--- app/HBSensorUIApp.js
+++ app/HBSensorUIApp.js
@@ -3,7 +3,7 @@ var HBSensorUIApp = angular.module('HBSensorUIApp', []);
 HBSensorUIApp.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
   $locationProvider.html5Mode(false);
   $routeProvider
-    .when('/', {templateUrl: '/partials/carousel.html', controller: 'MainPageCtrl'})
+    .when('/', {templateUrl: '/partials/main.html', controller: 'MainPageCtrl'})
     .when('/view/:identifier', {templateUrl: '/partials/view-toon.html', controller: 'ViewToonPageCtrl'})
     .otherwise({redirectTo: '/'})
     ;
@@ -20,14 +20,13 @@ function HBSensorUIAppCtrl($scope, $http, $rootScope) {
         .success(function(data, status) {
             console.log("Overview:");
             console.dir(data);
+            var map = [];
             _.each(data, function(prof) {
                 prof.Identifier = prof.Name + "-" + prof.Faction + "-" + prof.Realm;
+                map[prof.Identifier] = prof;
             });
             $scope.profileOverviews = data;
-            $scope.profileMap = [];
-            _.each(data, function(prof) {
-                $scope.profileMap[prof.Identifier] = prof;
-            });
+            $scope.profileMap = map;
             //http://us.battle.net/api/wow/character/saurfang/lirienne
             //http://us.battle.net/static-render/us/saurfang/219/96037083-avatar.jpg
             //cache using amplify?
@@ -50,8 +49,54 @@ function MainPageCtrl($scope, $http, $rootScope) {
 
 function ViewToonPageCtrl($scope, $http, $rootScope, $routeParams) {
     $scope.identifier = $routeParams.identifier;
-    $scope.$watch('profileOverviews', function() {
-        if (!$scope.profileOverviews) return;
+    $scope.$watch('profileMap', function() {
+        if (!$scope.profileMap) return;
         $scope.profile = $scope.profileMap[$scope.identifier];
+        $scope.profile.EXPData = {
+            title:{
+                text: "EXP Gain"
+            },
+            data: [//array of dataSeries
+                { //dataSeries object
+
+                    /*** Change type "column" to "bar", "area", "line" or "pie"***/
+                    type: "spline",
+                    dataPoints: [
+{ x: new Date(2012, 00, 1), y: 1352 },
+        { x: new Date(2012, 01, 1), y: 1514 },
+        { x: new Date(2012, 02, 1), y: 1321 },
+        { x: new Date(2012, 03, 1), y: 1163 },
+        { x: new Date(2012, 04, 1), y: 950 },
+        { x: new Date(2012, 05, 1), y: 1201 },
+        { x: new Date(2012, 06, 1), y: 1186 },
+        { x: new Date(2012, 07, 1), y: 1281 },
+        { x: new Date(2012, 08, 1), y: 1438 },
+        { x: new Date(2012, 09, 1), y: 1305 },
+        { x: new Date(2012, 10, 1), y: 1480 },
+        { x: new Date(2012, 11, 1), y: 1291 },
+                    ]
+                }
+            ]
+        };
     });
 }
+
+
+HBSensorUIApp.directive('chart', function() {
+  return {
+    restrict: 'A',
+    scope: {chartdata: '='},
+    link: function(scope, element, attrs) {
+        scope.$watch('chartdata', function() {
+            if (!scope.chartdata) return;
+            var chartElement = $(element);
+            if (!chartElement.attr("id")) {
+                chartElement.attr("id", _.uniqueId('chart_'));
+            }
+            var chart = new CanvasJS.Chart(chartElement.attr("id"), scope.chartdata);
+
+            chart.render();
+        });
+    }
+  }
+})
